ea8f400c0fc96a8ffddce23cef73465a3953b537
Aswanidevm <maswanidev@gmail.com>
2023-06-28 10:46:09 +0530

    main

--- ec2/main.tf
+++ ec2/main.tf
@@ -28,7 +28,6 @@ resource "aws_security_group" "sg" {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
   egress {
