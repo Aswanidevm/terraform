3d84d9adf8dd1d27adcc7c32ee5a6abb53ffa7bf
Aswanidevm <maswanidev@gmail.com>
2023-07-02 08:37:34 +0530

    variable

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,19 +4,17 @@ data "aws_ami" "ec2" {
   owners = ["973714476881"] 
 }
 
-
-
 resource "aws_instance" "ec2" {
-  for_each = var.instances
-  ami           = data.aws_ami.ec2.id
-  instance_type = lookup(each.value, "instance_type", "t3.micro")
+   ami           = data.aws_ami.ec2.id
+  instance_type = var.instance_type
   tags = {
-    Name = each.key
+    Name = var.name
   }
 
 }
 
 resource "null_resource" "ansible"{
+  depends_on = [ aws_instance.ec2, aws_route53_record.wwww]
   provisioner "remote-exec" {
 
   connection {
@@ -34,18 +32,16 @@ resource "null_resource" "ansible"{
   }
 }
 
-resource "aws_route53_record" "dns-record" {
-  for_each = var.instances
+resource "aws_route53_record" "wwww" {
   zone_id = "Z04818282BOE8RVGV13K7"
-  name    = "${each.value["name"]}"
+  name    = "${var.name}"
   type    = "A"
   ttl     = "300"
   records = ["aws_instance.ec2.private.ip"]
 }
 
 resource "aws_security_group" "sg" {
-  for_each = var.instances
-  name        = "${each.value["name"]}-sg"
+  name        = "${var.name}-sg"
   description = "Allow TLS inbound traffic"
 
 
@@ -66,61 +62,11 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = each.value["name"]
+    Name = var.name
   }
 }
 
 
-variable instances{
-  default = {
-    frontend = {
-      instance_type = "t3.micro"
-      name = "frontend"
-   }
-
-    mongodb = {
-      instance_type = "t3.micro"
-      name = "mongodb"
-   }
-
-    catalogue = {
-      instance_type = "t3.micro"
-      name = "catalogue"
-   }
-
-    mysql = {
-      instance_type = "t3.micro"
-      name = "mysql"
-  }
-
-    cart = {
-      instance_type = "t3.micro"
-      name = "cart"
-  }
-
-    shipping = {
-      instance_type = "t3.micro"
-      name = "shipping"
-  }
-
-    redis = {
-      instance_type = "t3.micro"
-      name = "redis"
-  }
-
-    rabbitmq = {
-      instance_type = "t3.micro"
-      name = "rabbitmq"
-  }
-
-    dispatch = {
-      instance_type = "t3.micro"
-      name = "dispatch"
-  }
-
-    user = {
-      instance_type = "t3.micro"
-      name = "user"
-  }
- }
-}
+variable name{}
+variable component{}
+variable instance_type{}
